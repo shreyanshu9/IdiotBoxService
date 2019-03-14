c9f1c044ea131d2dd3374b3fe3382c619b451a1f
Shreyanshu <ayush@ubuntu.lan.coriolis.co.in>
2019-03-14 11:58:19 +0530

    Idiot Box Service Project Initialization

---
+++ src/main/java/box/idiot/service/IdiotBoxServiceApplication.java
@@ -0,0 +1,13 @@
+package box.idiot.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdiotBoxServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdiotBoxServiceApplication.class, args);
+	}
+
+}
